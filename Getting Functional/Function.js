3c674968d2f8f702af3dfb6c65c482f3388e9fa4
ettill <egidoethyljoyfalco@gmail.com>
2025-03-20 21:18:17 +0800

    Functions

---
+++ Getting Functional/Function.js
@@ -0,0 +1,45 @@
+// var dogName = "ice";
+// var dogWeight = 10;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+// dogName = "kopiko";
+// dogWeight = 23;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+// dogName = "panda";
+// dogWeight = 28;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+// dogName = "chester";
+// dogWeight = 5;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " says WOOF WOOF");
+    } else {
+        console.log(name + " says woof woof");
+    }
+}
+
+
+bark("digong", 23);
+bark("pakie", 15);
+bark("jackie", 25);
+bark("budoy", 18);
